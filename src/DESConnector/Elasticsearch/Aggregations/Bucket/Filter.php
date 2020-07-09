b04c20be640c993116584db4fcfcbcbe1eb88233
Ruben <ruben@your-source.nl>
2020-07-09 10:52:18 +0200

    Add filtered aggregation

--- src/DESConnector/Elasticsearch/Aggregations/Bucket/Filter.php
+++ src/DESConnector/Elasticsearch/Aggregations/Bucket/Filter.php
@@ -9,13 +9,69 @@ use nodespark\DESConnector\Elasticsearch\Aggregations\AggregationInterface;
  *
  * @package nodespark\DESConnector\Elasticsearch\Aggregations\Bucket
  */
-class Filter implements AggregationInterface
+class Filter extends Bucket
 {
-    const TYPE = 'filter';
+    const TYPE = 'terms';
 
-    // TODO: To be implemented as special case!
+    protected $size;
+    protected $order;
+    protected $search = array();
+    protected $filters = array();
 
-  public function getName() {
-    // TODO: Implement getName() method.
-  }
+    public function setSize($size)
+    {
+        $this->size = $size;
+    }
+
+    public function setOrder($order)
+    {
+        $this->order = $order;
+    }
+
+    public function setSearch($search)
+    {
+        $this->search = $search;
+    }
+
+    public function addFilters($filters)
+    {
+        $this->filters[] = $filters;
+    }
+
+    public function constructAggregation()
+    {
+        $aggregation = parent::constructAggregation();
+
+        // Set the additional parameters if needed.
+        if (isset($this->size)) {
+            $aggregation[$this->name][static::TYPE]['size'] = $this->size;
+        }
+
+        $aggregation[$this->name . '_global']['aggs'][$this->name]['aggs'][$this->name] = $aggregation[$this->name];
+
+        $aggregation[$this->name . '_global']['aggs'][$this->name]['filter']['bool'] = array();
+        if (!empty($this->search)) {
+          $aggregation[$this->name . '_global']['aggs'][$this->name]['filter']['bool'] = $this->search['bool'];
+        }
+
+        if (!empty($this->filters)) {
+          foreach ($this->filters as $filter) {
+            if (!empty($filter['bool'])) {
+              $aggregation[$this->name . '_global']['aggs'][$this->name]['filter']['bool'] += $filter['bool'];
+            }
+            elseif (!empty($filter[0]['bool'])) {
+              $aggregation[$this->name . '_global']['aggs'][$this->name]['filter']['bool'] += $filter[0]['bool'];
+            }
+          }
+        }
+
+        if (!empty($aggregation[$this->name . '_global']['aggs'][$this->name]['filter']['bool'])) {
+          $aggregation[$this->name . '_global']['global'] = (Object)array();
+        }
+        else {
+          unset($aggregation[$this->name . '_global']['aggs'][$this->name]['filter']);
+        }
+
+        return $aggregation;
+    }
 }
